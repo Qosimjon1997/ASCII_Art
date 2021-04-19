1cc4fdbe10cd3a61b2c98803c760b1f36117addb
Qosimjon <33579671+Qosimjon1997@users.noreply.github.com>
2021-04-19 22:50:19 +0500

    Add project files.

---
+++ ASCII_Art/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCII_Art
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 2.5;
+        private const int maxWidth = 350;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.WriteLine("Press enter to start...\n");
+
+            while(true)
+            {
+                Console.ReadLine();
+
+                if(openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscala();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                
+                foreach(var row in rows)
+                {
+                    Console.WriteLine(row);
+                }
+
+                var rowNegative = converter.ConvertAsNegative();
+
+                File.WriteAllLines("images.txt", rows.Select(r => new string(r)));
+
+                Console.SetCursorPosition(0, 0);
+
+            }
+
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            
+            var newHeight = bitmap.Height / WIDTH_OFFSET * maxWidth / bitmap.Width;
+            if(bitmap.Width > maxWidth || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            }
+            return bitmap;
+        }
+
+
+    }
+}
